25df358c0bdebaee33ab6109d73047a20b2746aa
Nanaryu <filipqqq1234@gmail.com>
2023-04-21 22:09:08 +0200

    działa ale przeciwnicy chyba nie

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import msvcrt
 from time import sleep
 import colorama
 from colorama import Fore
+from secrets import randbelow
+
 colorama.init()
 
-w = Fore.BLACK + "██"
-p = Fore.CYAN + "██"
+w = Fore.BLACK + "■ "
+p = Fore.CYAN + "■ "
 e = "  "
-x = Fore.GREEN + "▒▒"
+x = Fore.GREEN + "■ "
+v = Fore.RED + "■ " 
 
 room1 = [
     [w, w, w, w, w, x, x, x, x, x, w, w, w, w, w],
@@ -18,7 +21,7 @@ room1 = [
     [w, w, w, w, w, e, e, e, e, e, w, w, w, w, w],
     [x, e, e, e, e, e, e, e, e, e, e, e, e, e, x],
     [x, e, e, e, e, e, e, e, e, e, e, e, e, e, x],
-    [x, e, e, e, e, e, e, p, e, e, e, e, e, e, x],
+    [x, e, e, e, e, e, e, e, e, e, e, e, e, e, x],
     [x, e, e, e, e, e, e, e, e, e, e, e, e, e, x],
     [x, e, e, e, e, e, e, e, e, e, e, e, e, e, x],
     [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
@@ -36,7 +39,7 @@ room2 = [
     [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
     [x, e, e, e, e, e, e, e, e, e, e, e, e, e, x],
     [x, e, e, e, e, e, e, e, e, e, e, e, e, e, x],
-    [x, e, e, e, e, e, e, p, e, e, e, e, e, e, x],
+    [x, e, e, e, e, e, e, e, e, e, e, e, e, e, x],
     [x, e, e, e, e, e, e, e, e, e, e, e, e, e, x],
     [x, e, e, e, e, e, e, e, e, e, e, e, e, e, x],
     [w, w, w, w, w, e, e, e, e, e, w, w, w, w, w],
@@ -46,7 +49,12 @@ room2 = [
     [w, w, w, w, w, x, x, x, x, x, w, w, w, w, w]
 ]
 
-c_room = room1
+roomlist = [room1, room2]
+
+c_room = roomlist[0]
+p_cpos = (7, 7)
+px = p_cpos[0]
+py = p_cpos[1]
 
 def show_room(room):
     for row in room:
@@ -54,44 +62,103 @@ def show_room(room):
             print(x, end='')
         print("")
 
-def load_room(room):
-    current_room = room
-    return current_room
+def reset(room):
+    global e
+    global v
+    for row in room:
+        for element in row:
+            if element == p or element == v:
+                room[room.index(row)][row.index(element)] = e
+    room[7][7] = p
 
-def move_handle(room):
-    '''
+def game_handler(room):
+    global c_room
+    global px
+    global py
+    global v
+    global e
     if msvcrt.kbhit():
         key = msvcrt.getch() 
-        if key == b'w': 
-            for row in room:
-                for element in row:
-                    if element == p:
-                        if room.index(row) - 1 == 0:
-                            load_room(2)
-                        else:
-                            room[room.index(row) - 1][row.index(element)] = p
-                            row[row.index(p)] = e
-                    else:
-                        pass
-    '''
-    pass
+        if key == b'w':
+            if px > 0 and c_room[px - 1][py] != w:
+                c_room[px][py] = e
+                c_room[px - 1][py] = p
+                px -= 1
+                if c_room[px - 1][py] == x:
+                    reset(c_room)
+                    c_room = roomlist[randbelow(len(roomlist) - 1)]
+                    spawn_enemies(c_room)
+                    px = 7
+                    py = 7
+                system('cls')
+                show_room(c_room)
+
+        elif key == b's':
+            if px < 14 and c_room[px + 1][py] != w:
+                c_room[px][py] = e
+                c_room[px + 1][py] = p
+                px += 1
+                if c_room[px + 1][py] == x:
+                    reset(c_room)
+                    c_room = roomlist[randbelow(len(roomlist) - 1)]
+                    spawn_enemies(c_room)
+                    px = 7
+                    py = 7
+                system('cls')
+                show_room(c_room)
+
+        elif key == b'a':
+            if py > 0 and c_room[px][py - 1] != w:
+                c_room[px][py] = e
+                c_room[px][py - 1] = p
+                py -= 1
+                if c_room[px][py - 1] == x:
+                    reset(c_room)
+                    c_room = roomlist[randbelow(len(roomlist) - 1)]
+                    spawn_enemies(c_room)
+                    px = 7
+                    py = 7
+                system('cls')
+                show_room(c_room)
+
+        elif key == b'd':
+            if py < 14 and c_room[px][py + 1] != w:
+                c_room[px][py] = e
+                c_room[px][py + 1] = p
+                py += 1
+                if c_room[px][py + 1] == x:
+                    reset(c_room)
+                    c_room = roomlist[randbelow(len(roomlist) - 1)]
+                    spawn_enemies(c_room)
+                    px = 7
+                    py = 7
+                system('cls')
+                show_room(c_room)
+        for row in c_room:
+            for element in row:
+                if element == v:
+                    if row.index(element) < py:
+                        room[room.index(row)][row.index(element)] = e
+                        room[room.index(row)][row.index(element) + 1] = v
+                    elif row.index(element) > py:
+                        room[room.index(row)][row.index(element)] = e
+                        room[room.index(row)][row.index(element) - 1] = v
+
+def spawn_enemies(room):
+    global e
+    global v
+    for row in room:
+        for element in row:
+            if element == e:
+                if randbelow(50) == 0:
+                    room[room.index(row)][row.index(element)] = v
+                else:
+                    pass
+
+
+c_room[px][py] = p
+show_room(c_room)
 
 while True: 
-    move_handle(c_room)
-    if msvcrt.kbhit():
-        key = msvcrt.getch() 
-        if key == b'w': 
-            for row in c_room:
-                for element in row:
-                    if element == p:
-                        if c_room.index(row) - 1 == 0:
-                            load_room(room2)
-                        else:
-                            c_room[c_room.index(row) - 1][row.index(element)] = p
-                            row[row.index(p)] = e
-                    else:
-                        pass
-    show_room(c_room)
-    sleep(0.1)
-    system('cls')
+    game_handler(c_room)
     
